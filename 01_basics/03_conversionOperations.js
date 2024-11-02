f673988da9c01f692cfe8e16196849bd27bf8e78
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2024-11-02 22:14:16 +0000

    updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,9 +1,9 @@
 let score = "33ab"
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let value = Number(score) 
-console.log(value);
+let value1 = Number(score) 
+//console.log(value);
 
 //"33" => 33
 //"33and" => NaN
@@ -11,10 +11,32 @@ console.log(value);
 
 let isLoggedin=1
 let booleanIsLoggedIn=Boolean(isLoggedin)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //""=>false
 //"abs"=>true
+
+//********operations***********/
+
+let value=3
+let negvalue= -value
+// console.log(negvalue);
+
+let str="hello"
+let ste=" world"
+let str3=str+ste;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+let num1,num2,num3
+let counter=100
+counter++
+console.log(counter);
 
 
