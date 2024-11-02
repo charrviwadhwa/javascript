d2f9b2f5bb19c3a39e01653b77c8b2f170356b6a
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2024-11-02 21:49:01 +0000

    uppdated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail="charvi@google.com"
+var accountpassword="12345"
+accountcity="Jaipur"
+let accountState;
+//accountId=2
+
+accountEmail="gdjh@google.com"
+accountpassword="51666"
+accountcity="delhi"
+console.log(accountId);
+
+console.log([accountEmail,accountpassword,accountcity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) // using node js
+
+console.log(3+3)
+console.log("hello")
+
+
+let name="hello"
+let age=45
+let isloggedin = false
+
+//number=>2 to power 53
+//null=stanalone value (object)
+//undefined=>
+//symbol => unique
+
+//object
+console.log(typeof null)
