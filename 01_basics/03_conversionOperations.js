f07acada055dec1e876b06ab819c0649152ee0f5
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2024-11-02 22:00:02 +0000

    converssion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "33ab"
+console.log(typeof score);
+console.log(typeof (score));
+
+let value = Number(score) 
+console.log(value);
+
+//"33" => 33
+//"33and" => NaN
+///true => 1
+
+let isLoggedin=1
+let booleanIsLoggedIn=Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//""=>false
+//"abs"=>true
+
+
