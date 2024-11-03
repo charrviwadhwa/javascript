fced1749a4430e0e66ed68ed78e096c10cf2b4e4
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2024-11-03 20:55:52 +0000

    updated

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const num =400
+const score = new Number(100)
+// console.log(score);
+
+
+// console.log(score.toString());
+// console.log(score.toFixed(2)); //shows pprecise value upto 2 decimal places
+
+const n = 45.2564
+// console.log(n.toPrecision(4));
+
+const hundred = 1000000000
+// console.log(hundred.toLocaleString('en-IN')); //put  commas in indian system
+
+
+//*****************maths*********************/
+
+// console.log(Math);
+// console.log(Math.abs(4));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10+1));
+
+const min=10
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
