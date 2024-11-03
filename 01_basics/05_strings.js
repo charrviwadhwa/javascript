9f469a03fb3a3ae8e0742cd0c1377f83f1732f05
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2024-11-03 20:36:17 +0000

    updated file

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,27 @@
+const name="hello"
+const count=23
+// console.log(name+count+"value");
+// console.log(`hello my name is ${name} and my repo count is ${count}`);
+
+const name1=new String ('hello')
+
+// console.log(name1[0]);
+// console.log(name1.__proto__);
+
+// console.log(name1.toUpperCase());
+// console.log(name.indexOf('l'));
+
+const str=name.slice(-2,5);
+// console.log(str);
+const newname = "  hello  "
+// console.log(newname.trim());
+
+const url = "https://google.com"
+// console.log(url.replace('g','G'));
+
+const str1="hello world"
+console.log(str.split(" "));
+
+
+
+
