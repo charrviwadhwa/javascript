72ed3f5e9d3af8b092d4dd28bb9cf308f660dc1c
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2024-11-26 08:48:11 +0000

    updated

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+//if 
+if(2=="2"){
+
+    console.log("executed");
+}
+const balance=1000
+// if(balance > 500) console.log("ttest"),
+// console.log("test1");
+
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,39 @@
+const email=[]
+if(email)
+{
+    console.log("got");  
+}
+else{
+    console.log("didn't get");  
+}
+
+//falsy value
+//false, 0, -0, Bigint 0n, "", null, undefined,NaN
+
+//truthy values
+//"0" , 'false', " " , [], {}, function(){}
+
+// if(email.length===0)
+// {
+//     console.log("array is empty");
+// }
+
+const emptyobj={}
+if(Object.keys(emptyobj).length===0)
+{
+    console.log("onject is empty");
+    
+}
+
+//Nullish Coalescing operator (??):null undefined
+let val1;
+///val1=5 ?? 10
+//val1=null??10
+
+val1=undefined??10
+console.log(val1);
+
+//Ternary Operator
+const price =100
+price<=80 ? console.log("less than 80"):console.log("more than 80");
+
